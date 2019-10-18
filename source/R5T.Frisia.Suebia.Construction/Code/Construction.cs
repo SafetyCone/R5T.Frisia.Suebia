7e0a7d3e7e40cde992ec39553ee33040134439dd
MinexAutomation <minex.automation@gmail.com>
2019-10-18 15:17:38 -0700

    Added service provider configuration. Added test of getting AWS EC2 server secrets from a serialization.

---
+++ source/R5T.Frisia.Suebia.Construction/Code/Construction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using R5T.Frisia.Suebia.Extensions;
+
+
+namespace R5T.Frisia.Suebia.Construction
+{
+    public static class Construction
+    {
+        public static void SubMain()
+        {
+            Construction.TestGetExampleAwsEc2ServerSecrets();
+        }
+
+        private static void TestGetExampleAwsEc2ServerSecrets()
+        {
+            var serverSecrets = Construction.GetExampleAwsEc2ServerSecrets();
+        }
+
+        private static AwsEc2ServerSecrets GetExampleAwsEc2ServerSecrets()
+        {
+            var serverSecretsSerialization = Construction.GetExampleAwsEc2ServerSecretsSerialization();
+
+            var serverSecrets = serverSecretsSerialization.GetAwsEc2ServerSecrets("Temp1");
+            return serverSecrets;
+        }
+
+        private static AwsEc2ServerSecretsSerialization GetExampleAwsEc2ServerSecretsSerialization()
+        {
+            var serialization = new AwsEc2ServerSecretsSerialization()
+                .AddHostConnectionDefinition("Temp1", "www.url.com", "User1")
+                .AddUserAuthentication("User1", "da-user", "da-pass", "/da/file.key")
+                ;
+
+            return serialization;
+        }
+    }
+}
--- source/R5T.Frisia.Suebia.Construction/Code/Program.cs
+++ source/R5T.Frisia.Suebia.Construction/Code/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Jutland;
+using R5T.Jutland.Newtonsoft;
+
+
 namespace R5T.Frisia.Suebia.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Construction.SubMain();
+        }
+
+        public static ServiceProvider GetServiceProvider()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IJsonFileSerializationOperator, NewtonsoftJsonFileSerializationOperator>()
+                .BuildServiceProvider()
+                ;
+
+            return serviceProvider;
         }
     }
 }
