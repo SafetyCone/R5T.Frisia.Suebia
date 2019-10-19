e62c0bddf7e10cb346e79b1e38bdc20f484b9b45
MinexAutomation <minex.automation@gmail.com>
2019-10-18 18:21:13 -0700

    Fully implemented Suebia-based AWS EC2 server secrets provider. Corrected the name of the *Suebia* based implementation.

---
+++ source/R5T.Frisia.Suebia/Code/Services/Classes/SuebiaAwsEc2ServerSecretsProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using R5T.Frisia.Suebia.Extensions;
+using R5T.Jutland;
+using R5T.Suebia;
+
+
+namespace R5T.Frisia.Suebia
+{
+    /// <summary>
+    /// Provides AWS EC2 server secrets from a secrets file using <see cref="R5T.Suebia.ISecretsFilePathProvider"/>.
+    /// </summary>
+    public class SuebiaAwsEc2ServerSecretsProvider : IAwsEc2ServerSecretsProvider
+    {
+        public IAwsEc2ServerSecretsFileNameProvider AwsEc2ServerSecretsFileNameProvider { get; }
+        public ISecretsFilePathProvider SecretsFilePathProvider { get; }
+        public IJsonFileSerializationOperator JsonFileSerializationOperator { get; }
+        public IAwsEc2ServerHostFriendlyNameProvider AwsEc2ServerHostFriendlyNameProvider { get; }
+
+
+        public SuebiaAwsEc2ServerSecretsProvider(
+            IAwsEc2ServerSecretsFileNameProvider awsEc2ServerSecretsFileNameProvider,
+            ISecretsFilePathProvider secretsFilePathProvider,
+            IJsonFileSerializationOperator jsonFileSerializationOperator,
+            IAwsEc2ServerHostFriendlyNameProvider awsEc2ServerHostFriendlyNameProvider)
+        {
+            this.AwsEc2ServerSecretsFileNameProvider = awsEc2ServerSecretsFileNameProvider;
+            this.SecretsFilePathProvider = secretsFilePathProvider;
+            this.JsonFileSerializationOperator = jsonFileSerializationOperator;
+            this.AwsEc2ServerHostFriendlyNameProvider = awsEc2ServerHostFriendlyNameProvider;
+        }
+
+        public AwsEc2ServerSecrets GetAwsEc2ServerSecrets()
+        {
+            var awsEc2ServerSecretsFileName = this.AwsEc2ServerSecretsFileNameProvider.GetAwsEc2ServerSecretsFileName();
+
+            var awsEc2ServerSecretsFilePath = this.SecretsFilePathProvider.GetSecretsFilePath(awsEc2ServerSecretsFileName);
+
+            var serverSecretsSerialization = this.JsonFileSerializationOperator.Deserialize<AwsEc2ServerSecretsSerialization>(awsEc2ServerSecretsFilePath);
+
+            var hostFriendlyName = this.AwsEc2ServerHostFriendlyNameProvider.GetHostFriendlyName();
+
+            var serverSecrets = serverSecretsSerialization.GetAwsEc2ServerSecrets(hostFriendlyName);
+            return serverSecrets;
+        }
+    }
+}
---
+++ source/R5T.Frisia.Suebia/Code/Services/Interfaces/IAwsEc2ServerHostFriendlyNameProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Frisia.Suebia
+{
+    public interface IAwsEc2ServerHostFriendlyNameProvider
+    {
+        string GetHostFriendlyName();
+    }
+}
