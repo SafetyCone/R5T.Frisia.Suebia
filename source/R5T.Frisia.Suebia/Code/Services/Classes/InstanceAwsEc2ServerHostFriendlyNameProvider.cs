29acc00f386eb482d5e021a371e0a6d35472b3b3
MinexAutomation <minex.automation@gmail.com>
2019-10-20 08:21:25 -0700

    Added host friendly name provider interface.

---
+++ source/R5T.Frisia.Suebia/Code/Services/Classes/InstanceAwsEc2ServerHostFriendlyNameProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace R5T.Frisia.Suebia
+{
+    public class InstanceAwsEc2ServerHostFriendlyNameProvider : IAwsEc2ServerHostFriendlyNameProvider
+    {
+        public string HostFriendlyName { get; }
+
+
+        public InstanceAwsEc2ServerHostFriendlyNameProvider(string hostFriendlyName)
+        {
+            this.HostFriendlyName = hostFriendlyName;
+        }
+
+        public string GetHostFriendlyName()
+        {
+            var hostFriendlyName = this.HostFriendlyName;
+            return hostFriendlyName;
+        }
+    }
+}
